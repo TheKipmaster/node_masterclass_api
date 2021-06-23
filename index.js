3dc4f43e15a0838884a80b2a1fe57fdbdecb110a
TheKipmaster <fkipman@hotmail.com>
2021-06-23 18:17:31 -0300

    Lesson 15

--- index.js
+++ index.js
@@ -2,10 +2,25 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
 
 // The Server should respond to all requests with a string
 const server = http.createServer(function(req, res) {
+  // Get the URL and parse it
+  const parsedUrl = url.parse(req.url, true);
+
+  // Get the path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+  // Get the HTTP method
+  const method = req.method.toLowerCase();
+
+  // Send the response
   res.end('Hello World\n');
+
+  // Log the request path
+  console.log('Request received on path: ' + trimmedPath + ' with this method: ' + method);
 
 });
 
