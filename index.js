4537a4796745fa9ad8efeea3be80595daabdfe63
TheKipmaster <fkipman@hotmail.com>
2021-06-23 18:43:12 -0300

    Lesson 18 - request infos received

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // Dependencies
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // The Server should respond to all requests with a string
 const server = http.createServer(function(req, res) {
@@ -13,14 +14,33 @@ const server = http.createServer(function(req, res) {
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g,'');
 
+  // Get the query string as an object
+  const queryStringObject = parsedUrl.query;
+
   // Get the HTTP method
   const method = req.method.toLowerCase();
 
-  // Send the response
-  res.end('Hello World\n');
+  // Get the headers as an object
+  const headers = req.headers;
 
-  // Log the request path
-  console.log('Request received on path: ' + trimmedPath + ' with this method: ' + method);
+  // Get the payload, if there is any
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
+
+  req.on('data', function(data) {
+    buffer += decoder.write(data);
+  });
+
+  req.on('end', function() {
+    buffer += decoder.end();
+
+    // Send the response
+    res.end('Hello World\n');
+
+    // Log the request path
+    console.log('Request received with this payload: ', buffer);
+  });
+
 
 });
 
