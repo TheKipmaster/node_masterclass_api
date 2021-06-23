098bd3b2514f2ed9a53770ec37b965bd240edad7
TheKipmaster <fkipman@hotmail.com>
2021-06-23 18:04:46 -0300

    Lesson 13

---
+++ index.js
@@ -0,0 +1,15 @@
+// Primary file for API
+
+// Dependencies
+const http = require('http');
+
+// The Server should respond to all requests with a string
+const server = http.createServer(function(req, res) {
+  res.end('Hello World\n');
+
+});
+
+// Start the server, have it listen on port 3000
+server.listen(3000, function() {
+  console.log('The server is listening on port 3000');
+});
